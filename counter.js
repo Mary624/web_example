ca6c8fb60ff03ce963bcf35e45a5617e576cff55
Maria Oreshnikova <mariaoreh777@gmail.com>
2022-09-03 23:46:10 +0500

    update form

--- counter.js
+++ counter.js
@@ -1,9 +1,13 @@
-import './style.css'
 
 function openForm() {
+    console.log("Aaa");
     document.getElementById("myForm").style.display = "block";
   }
   
   function closeForm() {
     document.getElementById("myForm").style.display = "none";
+    let section = document.createElement('section');
+    section.innerHTML = "<strong>Всем привет!</strong> Вы прочитали важное сообщение.";
+    let div = document.getElementById("main-div")
+    div.append(section);
   }
